140a33c169b17a77f99472c048c34f4ee29e576c
ajayrao021 <ajayguwani123@gmail.com>
2025-06-13 10:48:03 +0530

    Main.java

---
+++ .github/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Launch the login interface
+        System.out.println("=== Welcome to Library Book Issue System ===");
+
+        // Call the login class to begin
+        Login login = new Login();
+        login.startLogin();
+    }
+}
